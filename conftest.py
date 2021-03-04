013c1669e40d7fcb7307f4dc370e3e71bd9efec6
Sylvie Lamy-Thepaut <sylvie.lamy-thepaut@ecmwf.int>
2021-03-04 18:06:11 +0000

    Adding more information on the htnl report

--- conftest.py
+++ conftest.py
@@ -1,44 +1,74 @@
 import pytest
 import os
 
+
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item, call):
-    if os.environ.get("REGRESSION_MODE") == "off" :
+    if os.environ.get("REGRESSION_MODE") == "off":
         yield
         return
-    pytest_html = item.config.pluginmanager.getplugin('html')
+    pytest_html = item.config.pluginmanager.getplugin("html")
     outcome = yield
     report = outcome.get_result()
-    extra = getattr(report, 'extra', [])
-    if report.when == 'call':
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
         # always add url to report
         properties = {}
         for k, v in report.user_properties:
             properties[k] = v
-        img = 'file://{}/{}.png'.format(properties["output"], properties["test_name"])
-        
+        img = "file://{}/{}.png".format(properties["output"], properties["test_name"])
+
         if properties.get("new-test", True):
             extra.append(pytest_html.extras.html("<table>"))
-            extra.append(pytest_html.extras.html("<tr><th colspan='2'>new test : {} </th></tr>".format(properties["test_name"])))
+            extra.append(pytest_html.extras.html("<tr><th colspan='2'> Directory : {}   Python script : {}.py </th></tr>".format(properties["directory"], properties["test_name"])))
+            extra.append(
+                pytest_html.extras.html(
+                    "<tr><th colspan='2'>new test : {} </th></tr>".format(
+                        properties["test_name"]
+                    )
+                )
+            )
             extra.append(pytest_html.extras.html("<tr>"))
-            extra.append(pytest_html.extras.html("<td> <img src='{}' width='50%'/></td>".format(img)))
+            extra.append(
+                pytest_html.extras.html(
+                    "<td> <img src='{}' width='50%'/></td>".format(img)
+                )
+            )
             extra.append(pytest_html.extras.html("<tr>"))
             extra.append(pytest_html.extras.html("</table>"))
-    
+
         else:
-            diff = 'file://{}'.format(properties["diff-image"])
-            ref = 'file://{}'.format(properties["ref-image"])
+            diff = "file://{}".format(properties["diff-image"])
+            ref = "file://{}".format(properties["ref-image"])
             extra.append(pytest_html.extras.html("<table>"))
-            extra.append(pytest_html.extras.html("<tr><th colspan='2'>Difference in pixels : {} Test : {} </th></tr>".format(properties.get("diff", "undef"), properties["test_name"])))
+            extra.append(pytest_html.extras.html("<tr><th colspan='4'> Directory : {}   Python script : {}.py </th></tr>".format(properties["directory"], properties["test_name"])))
+            extra.append(
+                pytest_html.extras.html(
+                    "<tr><th colspan='2'>Difference in pixels : {} Test : {} </th></tr>".format(
+                        properties.get("diff", "undef"), properties["test_name"]
+                    )
+                )
+            )
             extra.append(pytest_html.extras.html("<tr>"))
-            extra.append(pytest_html.extras.html("<td> <img src='{}' width='50%'/></td>".format(ref)))
-            extra.append(pytest_html.extras.html("<td> <img src='{}'  width='50%'/></td>".format(diff)))
-            extra.append(pytest_html.extras.html("<td> <img src='{}'  width='50%'/></td>".format(img)))
+            extra.append(
+                pytest_html.extras.html(
+                    "<td> <img src='{}' width='50%'/></td>".format(ref)
+                )
+            )
+            extra.append(
+                pytest_html.extras.html(
+                    "<td> <img src='{}'  width='50%'/></td>".format(diff)
+                )
+            )
+            extra.append(
+                pytest_html.extras.html(
+                    "<td> <img src='{}'  width='50%'/></td>".format(img)
+                )
+            )
             extra.append(pytest_html.extras.html("<tr>"))
             extra.append(pytest_html.extras.html("</table>"))
-        xfail = hasattr(report, 'wasxfail')
+        xfail = hasattr(report, "wasxfail")
         if (report.skipped and xfail) or (report.failed and not xfail):
             # only add additional html on failure
-            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+            extra.append(pytest_html.extras.html("<div>Additional HTML</div>"))
         report.extra = extra
-
--- make_reference.py
+++ make_reference.py
@@ -14,31 +14,27 @@ import glob
 import subprocess
 
 
-
 def test_python(test_name, directory, output, reference):
-        os.chdir(directory)
+    os.chdir(directory)
 
+    output_name = "{}.png".format(test_name)
 
-        output_name = "{}.png".format(test_name)
-        
-        # run the test
-        try :
-            subprocess.check_call(["python3",  "{}.py".format(test_name)])
-        except Exception as e:
-            print (e)
-            assert False
+    # run the test
+    try:
+        subprocess.check_call(["python3", "{}.py".format(test_name)])
+    except Exception as e:
+        print(e)
+        assert False
 
-        output_exists = os.path.isfile(output_name)
-        assert output_exists == True
-        os.rename(output_name, os.path.join(reference, output_name))
+    output_exists = os.path.isfile(output_name)
+    assert output_exists == True
+    os.rename(output_name, os.path.join(reference, output_name))
 
-
-    
 
 # at import time so that pytest can find them
 
 
-DIR = os.environ.get('PWD', ".")
+DIR = os.environ.get("PWD", ".")
 
 for d in ["results", "reference"]:
     if not os.path.exists(d):
@@ -46,14 +42,18 @@ for d in ["results", "reference"]:
 
 os.chdir(DIR)
 for test_set in glob.glob("test/*"):
-    print (test_set)
-    try :
-        os.chdir(os.path.join(DIR,test_set))
+    print(test_set)
+    try:
+        os.chdir(os.path.join(DIR, test_set))
         for file_name in glob.glob("*.py"):
             test_name = os.path.splitext(file_name)[0]
-            method_name = "test_{}_{}".format(test_set,test_name)
+            method_name = "test_{}_{}".format(test_set, test_name)
             print("Adding test: {}".format(method_name))
-            test_python(test_name, os.path.join(DIR,test_set), os.path.join(DIR, "results"), os.path.join(DIR, "reference"))
+            test_python(
+                test_name,
+                os.path.join(DIR, test_set),
+                os.path.join(DIR, "results"),
+                os.path.join(DIR, "reference"),
+            )
     except:
         os.chdir(DIR)
-
