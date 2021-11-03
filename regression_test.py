bd29d415205b67e503f736d352ee0de47596942d
Sylvie <sylvie.lamy-thepaut@ecmwf.int>
2021-11-03 11:00:38 +0000

    Mark contour6 as expected to fail

--- regression_test.py
+++ regression_test.py
@@ -32,7 +32,7 @@ thresholds = {
    "plumes" : 3000,
 }
 
-skips =  [ "proj-regression-lambert_north_atlantic", "obsjson", "wind5", "wind6", "wind7", 
+skips =  [ "proj-regression-lambert_north_atlantic", "obsjson", "wind5", "wind6", "wind7", "contour6",
          "xarray1",  "xarray2",  "xarray3",  "xarray4",  "xarray5",  "xarray6",  "xarray7"]
 next_release = []
 tests=[]
